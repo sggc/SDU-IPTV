e54dc6c82a1db4f187ea35848ec45322318fcd61
plsy1 <zzy076525@gmail.com>
2025-11-11 00:50:23 +0800

    .

--- utils/merge.py
+++ utils/merge.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-def merge_m3u(input_files, search_group_title="山东频道"):
+def merge_m3u(input_files, output_file,search_group_title="山东频道"):
     first_file = input_files[0]
     if not os.path.exists(first_file):
         print(f"文件 '{first_file}' 不存在，跳过。")
@@ -28,16 +28,16 @@ def merge_m3u(input_files, search_group_title="山东频道"):
     with open(second_file, "r") as input:
         second_file_lines = input.readlines()
 
-    with open(input_files[0], "w") as output:
+    with open(output_file, "w") as output:
         output.writelines(lines[:insert_position])
         output.writelines(second_file_lines)
         output.write("\n")
         output.writelines(lines[insert_position:])
 
-    print(f"合并完成，输出到 {input_files[0]}")
+    print(f"合并完成，输出到 {output_file}")
 
 
-m3u_files = glob.glob("append/multicast/*.m3u")
+m3u_files = glob.glob("merge/multicast/local/*.m3u")
 
 city_dict = {}
 for filepath in m3u_files:
@@ -46,8 +46,12 @@ for filepath in m3u_files:
         city_dict[city] = []
     city_dict[city].append(filepath)
 
-multicast_dir = "multicast"
+multicast_dir = "merge/multicast/main"
+output_dir = "multicast"
 for city, append_files in city_dict.items():
     multicast_file = os.path.join(multicast_dir, f"multicast-{city}.m3u")
+    output_file = os.path.join(output_dir, f"multicast-{city}.m3u")
     all_files = [multicast_file] + append_files
-    merge_m3u(all_files)
+    merge_m3u(all_files,output_file)
+
+
